b54af04f624761ad998bc3dedd57db80699218e9
Hanzala Zahid <61132429+HanzalaZahid@users.noreply.github.com>
2022-07-30 02:59:28 +0500

    Transactions ADD AND EDIT

--- assets/js/behaviour.js
+++ assets/js/behaviour.js
@@ -4,8 +4,10 @@ $(document).ready(
         let allDialog = document.querySelectorAll('.dialog');
         let editClientDialog = document.querySelector('.edit_client_dialog');
         let editProjectDialog = document.querySelector('.edit_project_dialog');
+        let editTransactionDialog = document.querySelector('.edit_transaction_dialog');
         let addClientDialog = document.querySelector('.add_client_dialog');
         let addProjectDialog = document.querySelector('.add_project_dialog');
+        let addTransactionDialog = document.querySelector('.add_transaction_dialog');
         let deleteWarningDialog = document.querySelector('.delete_warning_dialog');
         $('.clients .button_list_change').click(
             function()
@@ -19,6 +21,12 @@ $(document).ready(
                 editProjectDialog.style.display = 'flex';
             }
         )
+        $('.transactions .button_list_change').click(
+            function()
+            {
+                editTransactionDialog.style.display = 'flex';
+            }
+        )
 
         $('.button_list_delete').click(
             function()
@@ -29,7 +37,7 @@ $(document).ready(
         $('.button_dialog_close').click(
             function()
             {
-                console.log(allDialog);
+                // console.log(allDialog);
                 allDialog.forEach(
                     function(box)
                     {
@@ -50,11 +58,48 @@ $(document).ready(
                 addProjectDialog.style.display = 'flex';
             }
         )
+        $('.button_add_transaction').click(
+            function()
+            {
+                addTransactionDialog.style.display = 'flex';
+            }
+        )
         $('.button_cancel_delete').click(
             function()
             {
                 deleteWarningDialog.style.display = 'none';;
             }
         )
+
+
+
+
+        // RADIO FOR TRANSACTION CHOICE
+        // RADIO FOR TRANSACTION CHOICE
+
+        let cashRadioNew = addTransactionDialog.querySelector('.cash_radio');
+        let accountRadioNew = addTransactionDialog.querySelector('.account_radio');
+        let cashRadio = editTransactionDialog.querySelector('.cash_radio');
+        let accountRadio = editTransactionDialog.querySelector('.account_radio');
+        cashRadioNew.addEventListener('click', function()
+        {
+            $('#select_new_account').hide();
+            $('#select_new_transaction').show();
+        })
+        accountRadioNew.addEventListener('click', function()
+        {
+            $('#select_new_account').show();
+            $('#select_new_transaction').hide();
+        })
+        cashRadio.addEventListener('click', function()
+        {
+            $('#select_account').hide();
+            $('#select_transaction').show();
+        })
+        accountRadio.addEventListener('click', function()
+        {
+            $('#select_account').show();
+            $('#select_transaction').hide();
+        })
     }
 )
